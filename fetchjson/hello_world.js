443d2b3611b950a260955ca40e366d3f6447f258
SzyClipse <elliot.johns@runbox.com>
2024-10-04 15:29:25 +0200

    missed my hello_world

---
+++ fetchjson/hello_world.js
@@ -0,0 +1,4 @@
+var hello = "Hello";
+var world = "World";
+var hello_world = hello + " " + world + "!";
+console.log(' -> ' + hello_world);
---
+++ fetchjson/hello_world.ts
@@ -0,0 +1,6 @@
+const hello: string = "Hello"
+const world: string = "World"
+
+var hello_world: string = hello + " " + world + "!"
+
+console.log(' -> ' + hello_world)
